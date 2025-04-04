bbb324087a9f532c5538e10766f10ebc2b04c39a
Kumar Bishwajeet <bishwajeetk23@gmail.com>
2025-04-04 10:24:14 +0000

    Updated typescript

--- Typescript/ReadMe.md
+++ Typescript/ReadMe.md
@@ -1,1 +1,38 @@
-TypeScript Resources
+# TypeScript Resources
+### setup typescript
+
+##  Steps
+        npm i typescript
+        npx tsc --init
+        change rootDir and OutDir form tsconfig file.
+## Types
+    Number - `number`
+    String - `string`
+    Boolean - `boolean`
+    Null 
+    Udefined
+    Void
+    Object
+    Array
+    Tuple
+    Any - skip the typechecking
+    Never
+Note :- Use compiler flag `noImplicitAny` to flag any implicit any as an error
+#### Type inference
+    creating a variable and assigning it to a particular value, TypeScript will use the value as its type
+Function
+#### Type Alias
+    A type alias is exactly that - a name for any type
+        Syntax:
+        type Point = {
+            x:number;
+            y:number;
+        }
+
+        If id  would be array, can we push values to array in case of READONLY,?
+
+#### Union
+    It give the power of mixing the data types and froming own data type using pipe operator
+#### Tuples
+     
+#### Enum 
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,9 +1,70 @@
+// Generator function
 function* sum(){
     yield 3;
     yield 4;
     yield 5;
 }
+
+// return generator object Object [Generator] {}
 const get = sum();
 for(let o of get){
     console.log(o);
+}
+
+// type number
+function t_sum(a:number,b:number):number{
+    return a+b;
+}
+
+// By default value provided in argument itself
+function user(name: string, email : string, isPair : boolean = false){
+    return {
+        name,
+        email,
+        isPair
+    };
+}
+console.log(user("Kumar Bishwajeet","kumar@adani.com"));
+
+// Objects
+function createUser(user:{name:string,email:string,isPaid:boolean}):{name:string,email:string,isPaid:boolean}{
+    return user;
+}
+
+const user1 =  {
+    name :"Kumar",
+    email:"kumar@adani.com",
+    isPaid:false
+};
+console.log(createUser(user1));
+
+// Type Alias
+type Point = {
+    x:number;
+    y:number;
+}
+
+type cardDate = {
+    cardDate: string
+}
+type cardNumber = {
+    cardNumber: number
+}
+
+//  Defining a new type using previous types
+type cardDetails = cardDate & cardNumber & {
+    cvv: number
+}
+
+
+// Union - uses pipe operator
+let Myuser: number | string;
+
+function getDbId(id: number | string){
+    // here it will give error as it is only valid for string but id can be number or string 
+    // id.toLowerCase();
+
+    if(typeof id === "string"){
+        id.toLowerCase();
+    }
 }
